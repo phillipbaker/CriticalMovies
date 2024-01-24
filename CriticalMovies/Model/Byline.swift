4f2b20981d7bfc9c27c3f2f520b775ea952c6456
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2024-01-24 11:21:33 +0000

    Update Byline model and mapping logic to use structured author names instead of the original (print edition) byline String.

--- CriticalMovies/Model/Byline.swift
+++ CriticalMovies/Model/Byline.swift
@@ -8,9 +8,25 @@
 import Foundation
 
 struct Byline: Decodable {
-    let original: String
+    let authors: [Author]
     
     func mapToAuthors() -> String {
-        return String(original.dropFirst(3))
+        return authors
+            .compactMap { $0.firstName + " " + $0.lastName }
+            .joined(separator: ", ")
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case authors = "person"
+    }
+}
+
+struct Author: Decodable {
+    let firstName: String
+    let lastName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "firstname"
+        case lastName = "lastname"
     }
 }
